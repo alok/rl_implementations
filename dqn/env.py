d51644ddf5b32b38da9c991bc44912943038b6a5
Alok Singh <alokbeniwal@gmail.com>
2018-01-23 16:17:35 -0800

    Generalize action space size

--- dqn/env.py
+++ dqn/env.py
@@ -3,8 +3,7 @@
 
 import gym
 
-A = 2  # actions are discrete  (0 or 1)
-S = 4  # states are (4,) arr
-gamma = discount = .999
-
 env = gym.make('CartPole-v0')
+
+A = env.action_space.n  # actions are discrete  (0 or 1)
+S = 4  # states are (4,) arr

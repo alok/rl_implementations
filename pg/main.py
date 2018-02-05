7c7b311074b73c141c8d1dbb55b5df95a1e696a5
Alok Singh <alokbeniwal@gmail.com>
2018-02-04 19:15:39 -0800

    add network

--- pg/main.py
+++ pg/main.py
@@ -1,16 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import argparse
-import logging
-import os
 import random
-import re
-import shutil
-import subprocess
-import sys
-from logging import debug, info, log
-from pathlib import Path
 
 import gym
 import numpy as np
@@ -18,29 +9,68 @@ import torch
 import torch.nn.functional as F
 import torch.nn.init as init
 import torch.optim as optim
-import torchvision.datasets as dset
-import torchvision.transforms as transforms
-from torch import Tensor, nn
-from torch.autograd import Variable as V
+from torch import Tensor, distributions, nn
+from torch.autograd import Variable
 from torch.nn import Parameter as P
-from torch.utils.data import DataLoader, Dataset
+from torch.nn import Sequential
+from torch.utils.data import DataLoader, Dataset, TensorDataset
+
+from lib.replay_buffer import ReplayBuffer
 
 env = gym.make('CartPole-v0')
-iterations = 1000
 
-for iteration in range(iterations):
-    s: np.ndarray = env.reset()
-    done = False
+S, A = int(np.prod(env.observation_space.shape)), int(env.action_space.n)
 
-    while not done:
-        #
-        probs = pi(Variable(torch.from_numpy(s)))
-        a = probs.sample()
-        succ, r, done, _ = env.step(a)
+# for discrete action spaces only
+policy = nn.Sequential(
+    nn.Linear(S, 50),
+    nn.ReLU(),
+    nn.Linear(50, 50),
+    nn.ReLU(),
+    nn.Linear(50, A),
+    nn.Softmax(),
+)
+
+opt = optim.Adam(policy.parameters())
 
 
 
-        # TODO append data and train minibatch
+BUFFER_SIZE = 10**6
+BATCH_SIZE = 128
+GAMMA = 0.99
+NUM_EPISODES = 1000
+TARGET_UPDATE = 100
+EPS_START = 0.999
+EPS_END = 0.01
+EPS_DECAY = 200
+
+
+def eps_greedy(env, s, policy):
+
+    if random.random < eps:
+        a = env.action_space.sample()
+    else:
+        s = Variable(torch.from_numpy(s).float())
+        p = distributions.Categorical(policy(s))
+
+        a = p.sample()
+
+    return a
+
 
 if __name__ == '__main__':
-    pass
+    buffer = ReplayBuffer(BUFFER_SIZE)
+
+    for episode in range(NUM_EPISODES):
+        s: np.ndarray = env.reset()
+        done = False
+
+        while not done:
+            #
+            a = eps_greedy(s)
+            succ, r, done, _ = env.step(a)
+
+
+        R = sum(rewards)
+
+            # TODO append data and train minibatch

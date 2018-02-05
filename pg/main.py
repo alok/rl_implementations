21f80af53efce766bdfb8f7f2d695c8d76714a22
Alok Singh <alokbeniwal@gmail.com>
2018-02-04 20:01:23 -0800

    Implement vanilla policy gradient
    This only trains on whole trajectories, and doesn't do any value function
    estimate. It can solve CartPole in anywhere from 250-600 episodes.
    
    With minimal effort, it could be refactored to work in continuous action spaces or discrete state spaces.

--- pg/main.py
+++ pg/main.py
@@ -6,71 +6,65 @@ import random
 import gym
 import numpy as np
 import torch
-import torch.nn.functional as F
-import torch.nn.init as init
 import torch.optim as optim
-from torch import Tensor, distributions, nn
+from torch import Tensor, distributions
 from torch.autograd import Variable
-from torch.nn import Parameter as P
-from torch.nn import Sequential
-from torch.utils.data import DataLoader, Dataset, TensorDataset
+from torch.nn import Linear, ReLU, Sequential, Softmax
 
-from lib.replay_buffer import ReplayBuffer
+DISCOUNT = 0.99
+NUM_EPISODES = 1000
 
 env = gym.make('CartPole-v0')
 
 S, A = int(np.prod(env.observation_space.shape)), int(env.action_space.n)
 
 # for discrete action spaces only
-policy = nn.Sequential(
-    nn.Linear(S, 50),
-    nn.ReLU(),
-    nn.Linear(50, 50),
-    nn.ReLU(),
-    nn.Linear(50, A),
-    nn.Softmax(),
-)
+policy = Sequential(
+    Linear(S, 50),
+    ReLU(),
+    Linear(50, 50),
+    ReLU(),
+    Linear(50, A),
+    Softmax(),
+).cuda()
 
 opt = optim.Adam(policy.parameters())
 
 
-
-BUFFER_SIZE = 10**6
-BATCH_SIZE = 128
-GAMMA = 0.99
-NUM_EPISODES = 1000
-TARGET_UPDATE = 100
-EPS_START = 0.999
-EPS_END = 0.01
-EPS_DECAY = 200
-
-
-def eps_greedy(env, s, policy):
-
-    if random.random < eps:
-        a = env.action_space.sample()
-    else:
-        s = Variable(torch.from_numpy(s).float())
-        p = distributions.Categorical(policy(s))
-
-        a = p.sample()
-
-    return a
+def G(rewards, start=0, end=None):
+    return sum(rewards[start:end])
 
 
 if __name__ == '__main__':
-    buffer = ReplayBuffer(BUFFER_SIZE)
 
     for episode in range(NUM_EPISODES):
-        s: np.ndarray = env.reset()
-        done = False
+        s, done = env.reset(), False
+        rewards, log_probs = [], []
 
         while not done:
-            #
-            a = eps_greedy(s)
-            succ, r, done, _ = env.step(a)
+            s = Variable(torch.from_numpy(s).float()).cuda()
+            p = distributions.Categorical(policy(s))
+            a = p.sample()
+            succ, r, done, _ = env.step(int(a.data))
+            rewards.append(r)
+            log_probs.append(p.log_prob(a))
 
+            s = succ
 
-        R = sum(rewards)
+        discounted_rewards = [pow(DISCOUNT, t) * r for t, r in enumerate(rewards)]
+        cumulative_returns = [G(discounted_rewards, t) for t in range(len(discounted_rewards))]
 
-            # TODO append data and train minibatch
+        R = Variable(Tensor((cumulative_returns))).cuda()
+
+        log_probs = torch.stack(log_probs).view(-1)
+
+        loss = -(R @ log_probs) / len(rewards)
+
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+
+        print(f'ep: {episode}, R: {sum(rewards)}')
+
+        if sum(rewards) > 200 - 5:
+            print(30 * '-')

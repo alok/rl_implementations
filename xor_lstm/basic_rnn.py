9584bf5d7ea848ebbb1573331af1372e502c7571
Alok Singh <alokbeniwal@gmail.com>
2018-02-18 02:55:03 -0800

    Model actually seems to work now

--- xor_lstm/basic_rnn.py
+++ xor_lstm/basic_rnn.py
@@ -1,41 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import argparse
-import logging
+import itertools
 import operator
-import os
-import random
-import re
-import shutil
-import subprocess
-import sys
-from functools import reduce
-from itertools import accumulate
-from logging import debug, info, log
 from pathlib import Path
 
 import numpy as np
 import torch
 import torch.nn.functional as F
-import torch.nn.init as init
 import torch.optim as optim
-from torch import Tensor, distributions, nn
+from torch import Tensor, nn
 from torch.autograd import Variable
 from torch.nn import (BatchNorm1d, BatchNorm2d, Conv2d, Dropout, Dropout2d,
                       Linear, MaxPool2d, Parameter, ReLU, Sequential, Softmax)
 from torch.utils.data import DataLoader, Dataset, TensorDataset
 
+NUM_SAMPLES = 100_000
 INPUT_DIM = 1
 NUM_CLASSES = 2
 SEQ_LEN = 50
-BATCH_SIZE = 1
+BATCH_SIZE = 2
+OP = operator.xor
 
 
 def foldr(arr: np.ndarray, op) -> np.ndarray:
     """Specific version of foldr that's only for Numpy arrays"""
 
-    return np.fromiter(accumulate(arr, op), dtype=np.float32, count=len(arr))
+    return np.fromiter(itertools.accumulate(arr, op), dtype=np.float32, count=len(arr))
 
 
 # TODO generate minibatch in one shot
@@ -44,59 +35,77 @@ def foldr(arr: np.ndarray, op) -> np.ndarray:
 def gen_data(seq_len=SEQ_LEN, batch_size=BATCH_SIZE, input_dim=INPUT_DIM):
     xs = np.random.randint(low=0, high=2, size=(seq_len, batch_size, input_dim))
     # Have to do some weird slicing here to do the fold in one shot
-    ys = np.stack([foldr(xs[:, i, :], operator.xor) for i in range(xs.shape[1])])
+
+    # I was stacking along the wrong axis
+    ys = np.stack([foldr(xs[:, i, 0], OP) for i in range(xs.shape[1])], axis=-1)
 
     xs = Variable(torch.from_numpy(xs).float())
     ys = Variable(torch.from_numpy(ys).long())
-    return xs, ys
+
+    return xs.cuda(), ys.cuda()
 
 
-class rnn(nn.Module):
+class Model(nn.Module):
     def __init__(self):
         super().__init__()
-        self.h = Variable(torch.Tensor([0]))
-        self.fc1 = Linear(2, 1)
-        self.out = nn.functional.sigmoid
 
-    def forward(self, xs):
-        h = self.h
+        H = 20
 
-        for i, x in enumerate(xs.view(-1)):
-            h = self.fc1(torch.cat((x, h)))
+        self.lstm = nn.LSTM(
+            input_size=1,
+            hidden_size=H,
+            num_layers=3,
+        )
+        self.fc = nn.Linear(H, NUM_CLASSES)
 
-        y = self.out(h)
-        return y
+    def forward(self, x):
+        x, _ = self.lstm(x)
+        x = self.fc(x)
+        return x
 
 
-model = nn.LSTM(
-    input_size=1,
-    hidden_size=2,
-    num_layers=1,
-)
+def argmax(tensor, dim=1):
+    return tensor.max(dim=dim)[1]
+
+
+# Hack to check if we've already trained a model (assumed to be a good one.
+test_mode = Path('model.pth').exists()
+train_mode = not test_mode
+
+if test_mode:
+    model = torch.load('model.pth')
+    model.lstm.flatten_parameters()
+else:
+    model = Model().cuda()
 
 if __name__ == '__main__':
 
-    criterion = nn.functional.cross_entropy
+    criterion = F.cross_entropy
     optimizer = optim.Adam(model.parameters())
 
-    while True:
+    # TODO use variable len padded sequence
+
+    for i in range(NUM_SAMPLES // BATCH_SIZE):
 
         seq_len = np.random.randint(low=1, high=50 + 1)
-        batch_size = np.random.randint(low=1, high=32 + 1)
-        input_dim = INPUT_DIM
+        batch_size = np.random.randint(low=2, high=32 + 1) if test_mode else BATCH_SIZE
 
         xs, ys = gen_data(
             seq_len=seq_len,
             batch_size=batch_size,
-            input_dim=input_dim,
         )
 
-        predictions, hiddens = model(xs)
-        # predictions = nn.functional.softmax(predictions, dim=2)
+        predictions = model(xs)
 
         loss = criterion(predictions.view(-1, NUM_CLASSES), ys.view(-1))
-        print(loss.data[0])
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        if i % (NUM_SAMPLES // 200) == 0:
+            print(f'Iteration: {i} | Batch size: {batch_size} | Loss: %.5f' % float(loss))
+
+        if train_mode:
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+    if train_mode:
+        torch.save(model, 'model.pth')

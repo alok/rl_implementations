229d736c8cf1fb9c9b33dc54b481f1600769d697
Alok Singh <alokbeniwal@gmail.com>
2018-03-08 14:32:12 -0800

    WIP

--- mlsh/main.py
+++ mlsh/main.py
@@ -1,37 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import argparse
-import functools
 import itertools
-import logging
-import os
 import random
-import re
-import shutil
-import subprocess
-import sys
-import typing
-from functools import reduce
-from logging import debug, info, log
-from pathlib import Path
-from typing import (Any, Callable, Dict, List, NamedTuple, NewType, Sequence,
-                    TypeVar)
+from typing import Any
 
 import gym
 import numpy as np
 import torch
 import torch.nn.functional as F
-import torchvision.datasets as dset
-import torchvision.transforms as transforms
-from pudb import set_trace
-from torch import Tensor, distributions, nn
+from torch import distributions, nn
 from torch.autograd import Variable
-from torch.nn import BatchNorm1d, Dropout, Linear, ReLU, Sequential, Softmax
+from torch.nn import Linear
 from torch.optim import Adam
-from torch.utils.data import DataLoader, Dataset, TensorDataset
 
-# TODO do state and action spaces have to be same for all ends?
+from utils import ReplayBuffer, Step, np_to_var
 
 # In paper, ant was dropped into 9 envs, but its S,A were same. No transfer
 # learning yet.
@@ -39,40 +22,38 @@ from torch.utils.data import DataLoader, Dataset, TensorDataset
 State = Any
 Action = Any
 Timestep = int
-Policy: Callable[[State], Action]
 
 MAX_STEPS: Timestep = 1_000
-SUBPOLICY_DURATION: Timestep = 2_00
+SUBPOLICY_DURATION: Timestep = 200
 NUM_POLICIES = 10
+MAX_BUFFER_SIZE = 10**6
+BATCH_SIZE = 128
 
-envs = ['Ant-v2']
-envs = [gym.make(env) for env in envs]
-
-# TODO check that all envs have same state and action spaces
-# Start with random env
-env = random.choice(envs)
-S = int(np.prod(env.observation_space.shape))
-A = int(np.prod(env.action_space.shape))
-H = hidden_size = 50
-
-# S->num_policies
-# TODO try RNN?
-master = Sequential(
-    Linear(S, H),
-    ReLU(),
-    Linear(H, H),
-    ReLU(),
-    Linear(H, NUM_POLICIES),
-    Softmax(dim=0),
-)
-
-# TODO try returning distribution directly in forward() method
-def MasterPolicy(nn.Module):
+env_names = ['Ant-v2']
 
 
-# TODO return mean and variance for each
+class MasterPolicy(nn.Module):
+    """Returns categorical distribution over subpolicies."""
+
+    def __init__(self, state_size, hidden_size, output_size=NUM_POLICIES):
+        super().__init__()
+        S, H, O = state_size, hidden_size, output_size
+        self.fc1 = Linear(S, H)
+        self.fc2 = Linear(H, H)
+        self.out = Linear(H, O)
+
+    def forward(self, s):
+        s = F.relu(self.fc1(s))
+        s = F.relu(self.fc2(s))
+        s = F.softmax(self.out(s), dim=0)
+        P = distributions.Categorical(s)
+        return P
+
+
 class Policy(nn.Module):
-    def __init__(self):
+    def __init__(self, state_size, hidden_size, action_size):
+        super().__init__()
+        S, H, A = state_size, hidden_size, action_size
         self.fc1 = Linear(S, H)
         self.fc2 = Linear(H, H)
         self.mean_head = Linear(H, A)
@@ -81,49 +62,59 @@ class Policy(nn.Module):
     def forward(self, s):
         s = F.relu(self.fc1(s))
         s = F.relu(self.fc2(s))
-        a = F.relu(self.fc1(a))
         mean = self.mean_head(s)
         std = self.std_head(s)
         return mean, std
 
 
-policies = []
-
-
-def W(x: np.ndarray) -> Variable:
-    """Wrap a numpy array into a torch variable."""
-    return Variable(torch.from_numpy(s).float())
-
-
-s = env.reset()
-
-P = distributions.Categorical(master(W(s)))
-
-# TODO subpolicies should output parameters of Gaussian of the right action shape
-
 # first, implement policies and picking
 # then, implement all the reward and optimization
 
-# TODO think over what you're doing in terms of actually doing them, not in
-# terms of what you need to write on a computer
-
 # TODO turn loops into combinators so you can compose them
 # TODO loop over sampled tasks
-for t in itertools.count():
-    # pick new subpolicy every X steps
-    if t % SUBPOLICY_DURATION == 0:
-        policy = policies[int(P.sample())]
 
-    mean, std = policy(W(s))
-    p = distributions.Normal(mean, std)
-    a = p.sample()
-    succ, r, done, _ = env.step(a)
-    s = env.reset() if done else succ
+# TODO optimize each policy with something like PPO
+# loop through a whole rollout
+# TODO warmup for some number of timesteps
 
-    # TODO optimize each policy with something like PPO
-    # loop through a whole rollout
-    # TODO warmup for some number of timesteps
-    # TODO
+
+def rollout(env, start_state: State, policy, buffer, num_steps=SUBPOLICY_DURATION) -> None:
+    s = start_state
+
+    for i in range(num_steps):
+        mean, std = policy(np_to_var(s))
+        p = distributions.Normal(mean, std)
+        a = p.sample()
+        succ, r, done, _ = env.step(a.data.numpy())
+        env.done = done
+        buffer.append(Step(s, a, r, succ, done))
+        s = np_to_var(env.reset()) if done else succ
+
 
 if __name__ == '__main__':
-    pass
+
+    envs = [gym.make(env) for env in env_names]
+    # TODO assert that all envs have same state and action spaces
+    # Start with random env
+    for env in envs:
+        env.done = False
+    env = random.choice(envs)
+
+    S = int(np.prod(env.observation_space.shape))
+    A = int(np.prod(env.action_space.shape))
+    H = hidden_size = 50
+
+    policies = [Policy(S, H, A) for _ in range(NUM_POLICIES)]
+
+    buffer = ReplayBuffer(MAX_BUFFER_SIZE)
+
+    s = env.reset()
+
+    master = MasterPolicy(S, H, NUM_POLICIES)
+    P = master(np_to_var(s))
+
+    for t in itertools.count():
+        # pick new subpolicy every X steps
+        if t % SUBPOLICY_DURATION == 0:
+            policy = policies[int(P.sample())]
+        rollout(env, s, policy, buffer, SUBPOLICY_DURATION)

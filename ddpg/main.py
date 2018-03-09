d74e884bfa81469839e11a6cdb378647ee477c45
Alok Singh <alokbeniwal@gmail.com>
2018-03-08 23:43:54 -0800

    Add logging

--- ddpg/main.py
+++ ddpg/main.py
@@ -25,17 +25,47 @@ exp = Experiment(
 )
 
 parser = HyperOptArgumentParser(strategy='random_search')
-parser.add_argument('--num_steps', default=100_000, type=int, tunnable=False)
-parser.add_argument('--batch_size', default=64, type=int, tunnable=False)
-parser.add_argument('--num_steps', default=100_000, type=int, tunnable=False)
-parser.add_argument('--discount', default=0.995, type=float)
-parser.add_argument('--target_update', default=100, type=int)
-parser.add_argument('--soft_update_factor', default=.01, type=float)
-parser.add_argument('--hidden_size', default=50, type=int)
-parser.add_argument('--buffer_size', default=1_000_000, type=int)
-parser.add_argument('--noise_factor', default=1, type=float)
 
-args = parser.parse_args()
+parser.add_argument('--buffer_size', default=1_000_000, type=int)
+parser.add_argument('--num_steps', default=100_000, type=int)
+parser.add_argument('--hidden_size', default=50, type=int)
+
+parser.add_opt_argument_list(
+    '--batch_size',
+    default=64,
+    type=int,
+    tunnable=True,
+    options=[2**i for i in range(5, 9)],
+)
+
+parser.add_opt_argument_list(
+    '--discount',
+    default=0.995,
+    type=float,
+    tunnable=True,
+    options=[.9, .99, .995, .999],
+)
+
+parser.add_opt_argument_range(
+    '--target_update',
+    default=100,
+    type=int,
+    tunnable=True,
+    start=10,
+    end=100,
+    nb_samples=5,
+)
+
+parser.add_opt_argument_range(
+    '--noise_factor',
+    default=1,
+    type=float,
+    start=0,
+    end=1,
+    nb_samples=5,
+)
+
+hparams = parser.parse_args()
 
 env = gym.make('Pendulum-v0')
 
@@ -43,12 +73,12 @@ state_size = int(np.prod(env.observation_space.shape))
 action_size = int(np.prod(env.action_space.shape))
 
 S, A = state_size, action_size
-H = args.hidden_size
+H = hparams.hidden_size
 
 exp.add_metric_row({
     'S': state_size,
     'A': action_size,
-    'H': hidden_size,
+    'H': hparams.hidden_size,
 })
 
 
@@ -103,54 +133,65 @@ def format_batch(batch):
 def get_critic_train_data(succ_states, rewards, dones):
     # r + Q(s, pi(s'))
     Q_succ = critic_target(succ_states, actor_target(succ_states)).squeeze()
-    td_estimate = rewards + ((1 - dones) * DISCOUNT * Q_succ)
+    td_estimate = rewards + ((1 - dones) * hparams.discount * Q_succ)
     return td_estimate.detach()
 
 
 actor_opt = Adam(actor.parameters())
 critic_opt = Adam(critic.parameters())
 
-buffer = ReplayBuffer(BUFFER_SIZE)
+buffer = ReplayBuffer(hparams.buffer_size)
 s, rews = np_to_var(env.reset()), []
 
-for timestep in range(NUM_STEPS):
-    noise = Normal(mean=Variable(torch.zeros(A)), std=NOISE_FACTOR * Variable(torch.ones(A)))
+for hparam in hparams.trials(5):
+    exp.add_argparse_meta(hparam)
+    for timestep in range(hparam.num_steps):
+        noise = Normal(
+            mean=Variable(torch.zeros(A)), std=hparam.noise_factor * Variable(torch.ones(A))
+        )
 
-    if timestep % 1000 == 0:
-        NOISE_FACTOR /= 2
+        if timestep % 1000 == 0:
+            hparam.noise_factor /= 2
 
-    a = actor(s) + noise.sample()
-    succ, r, done, _ = env.step(a.data.numpy())
-    succ = np_to_var(succ)
-    buffer.append(Step(s, a, r, succ, done))
-    rews.append(r)
-    s = np_to_var(env.reset()) if done else succ
-    if done:
-        logging.info(f'step:{timestep + 1} | Loss: {-sum(rews)}')
-        rews = []
+        a = actor(s) + noise.sample()
+        succ, r, done, _ = env.step(a.data.numpy())
+        succ = np_to_var(succ)
+        buffer.append(Step(s, a, r, succ, done))
+        rews.append(r)
+        s = np_to_var(env.reset()) if done else succ
+        if done:
 
-    if len(buffer) >= BATCH_SIZE:
-        states, actions, rewards, succ_states, dones = format_batch(buffer.sample(BATCH_SIZE))
+            exp.add_metric_row({
+                'Timestep': timestep + 1,
+                'Loss': -sum(rews),
+            })
 
-        td_estims = get_critic_train_data(succ_states, rewards, dones)
+            rews = []
 
-        critic_preds = critic(states, actions.detach())
+        if len(buffer) >= hparam.batch_size:
+            states, actions, rewards, succ_states, dones = format_batch(
+                buffer.sample(hparam.batch_size)
+            )
 
-        critic_opt.zero_grad()
-        critic_loss = F.smooth_l1_loss(critic_preds, td_estims)
+            td_estims = get_critic_train_data(succ_states, rewards, dones)
 
-        critic_loss.backward()
-        critic_opt.step()
+            critic_preds = critic(states, actions.detach())
 
-        actor_opt.zero_grad()
-        actor_loss = -critic(states, actor(states)).mean()
+            critic_opt.zero_grad()
+            critic_loss = F.smooth_l1_loss(critic_preds, td_estims)
 
-        actor_loss.backward()
-        actor_opt.step()
+            critic_loss.backward()
+            critic_opt.step()
 
-        if timestep % TARGET_UPDATE == 0:
-            # Hard update
-            actor_target.load_state_dict(actor.state_dict())
-            critic_target.load_state_dict(critic.state_dict())
+            actor_opt.zero_grad()
+            actor_loss = -critic(states, actor(states)).mean()
 
-exp.save()
+            actor_loss.backward()
+            actor_opt.step()
+
+            if timestep % hparam.target_update == 0:
+                # Hard update
+                actor_target.load_state_dict(actor.state_dict())
+                critic_target.load_state_dict(critic.state_dict())
+
+    exp.save()
